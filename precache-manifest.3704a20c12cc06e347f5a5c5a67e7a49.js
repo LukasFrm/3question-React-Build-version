143b8e3ea32a5fc4147eca6ba9c82396352d23cf
LukasFrm <lukas.freimonas@gmail.com>
2019-08-16 10:26:57 +0300

    build commit

---
+++ precache-manifest.3704a20c12cc06e347f5a5c5a67e7a49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8300fed7914ecde12ba2632a1e73d365",
+    "url": "/index.html"
+  },
+  {
+    "revision": "492c3009659855c7942e",
+    "url": "/static/css/main.c3a1b679.chunk.css"
+  },
+  {
+    "revision": "d20357bb9f7803de031f",
+    "url": "/static/js/2.b1b049ec.chunk.js"
+  },
+  {
+    "revision": "492c3009659855c7942e",
+    "url": "/static/js/main.f54cfa67.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
